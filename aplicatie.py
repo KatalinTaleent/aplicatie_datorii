e0c7de8d4d5e8319fd9852fcd0e2bf249e069537
Kătălin Taleent <lucianthehacker@gmail.com>
2025-06-13 00:31:38 +0300

    functii de citire date si afisare continut fisier adaugate

--- aplicatie.py
+++ aplicatie.py
@@ -9,13 +9,27 @@
 
 """ cu aceasta functie incepe propriu-zis aplicatia
     fisierul in care apar datoriile este ,,datorii.txt'' """
-def initializeaza_fisier():
-#deschidem fisierul ,,datorii.txt''
+
+# deschidem fisierul ,,datorii.txt''
     fisier = open('datorii.txt', 'w')
 
-#aceasta functie permite citirea unei datorii de la tastatura, actualizand astfel fisierul
+# aceasta functie permite citirea unei datorii de la tastatura, obtinand date pentru actualizarea fisierului
 def citeste_datorie():
     nume_datorie = input("Introduceti numele datoriei: ")
     valoare_datorie = float(input("Introduceti valoarea in bani a datoriei: "))
     data_datorie = input("Cand a aparut aceasta datorie?")
-#TODO
+    este_achitata = input("Datoria este achitata? Alegeti intre Da si Nu")
+    data_achitare_datorie = "" # lasam gol aici deocamdata
+    if (este_achitata.casefold() == "Da".casefold()):
+        este_achitata = True
+    else: # presupunem ca datoria nu a fost achitata daca este alt raspuns decat Da/Nu
+        este_achitata = False
+    if este_achitata == True:
+        data_achitare_datorie = input("Precizati data la care ati scapat de datorie")
+    return [nume_datorie, valoare_datorie, data_datorie, este_achitata, data_achitare_datorie]
+
+    """ functia de mai jos afiseaza intreg continutul fisierului in care sunt retinute datoriile
+    """
+def afisare_datorii():
+    for line in fisier:
+        print(line)
